645ee7c1cdcbfd4565c6f3a01bd5ed3b7f2c864b
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-09-20 18:46:14 -0300

    fix: quase finalizado

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native';
 import { PickerItem } from './src/Components/Picker/Picker';
 import { api } from './src/api/urlApi';
 
 export default function App(){
-  const [listaMoedas, setListaMoedas] = useState([]);
+
   const [loading, setLoading] = useState(true);
+  const [moedas, seMoedas] = useState([]);
+  const [moedaSelecionada, setMoedaSelecionada] = useState(null);
 
   useEffect(()=> {
     async function loadMoedas() {
@@ -21,7 +23,8 @@ export default function App(){
         });
       });
 
-      setListaMoedas(arrayMoedas);
+      seMoedas(arrayMoedas);
+      setMoedaSelecionada(arrayMoedas[0].key);
       setLoading(false);
     }
 
@@ -39,8 +42,21 @@ export default function App(){
       <View style={styles.container}>
         <View style={styles.areaMoeda}>
           <Text style={styles.titulo}>Selecione Sua Moeda</Text>
-          <PickerItem />
+          <PickerItem
+            moedas={moedas}
+            moedaSelecionada={moedaSelecionada}
+            onChange={(moedaTrocada) => {
+              setMoedaSelecionada(moedaTrocada);
+            }}
+          />
+          <View style={styles.areaValor}>
+            <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+            <TextInput placeholder="Digite o Valor!" keyboardType="numeric" style={styles.input}/>
+          </View>
         </View>
+        <TouchableOpacity style={styles.areaBotao} onPress={()=> {}}>
+          <Text style={styles.textoBotao}>Converter</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -60,11 +76,38 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     padding: 8,
+    marginBottom: 1,
   },
   titulo: {
     color: '#000',
     fontSize: 16,
     fontWeight: '500',
     textAlign: 'center',
+  },
+  areaValor:{
+    width: '90%',
+    backgroundColor: '#F9F9F9',
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  input: {
+    width: '100%',
+    padding: 8,
+    fontSize: 18,
+    color: '#000',
+  },
+  areaBotao:{
+    width: '90%',
+    backgroundColor: '#FB4B57',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomRightRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  textoBotao:{
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
