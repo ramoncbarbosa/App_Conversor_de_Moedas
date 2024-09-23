1ba10094dca89d0bce1aaad291f1a9adfa9727b1
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-09-23 08:59:19 -0300

    fix: finale

--- App.jsx
+++ App.jsx
@@ -1,13 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import { PickerItem } from './src/Components/Picker/Picker';
 import { api } from './src/api/urlApi';
+import { reporter } from './metro.config';
 
 export default function App(){
 
   const [loading, setLoading] = useState(true);
-  const [moedas, seMoedas] = useState([]);
+  const [moedas, setMoedas] = useState([]);
   const [moedaSelecionada, setMoedaSelecionada] = useState(null);
+
+  const [moedaBValor, setMoedaBValor] = useState('')
+  const [valorMoeda, setValorMoeda] = useState(null)
+  const [valorConvertido, setValorConvertido] = useState(0)
+
 
   useEffect(()=> {
     async function loadMoedas() {
@@ -23,13 +29,26 @@ export default function App(){
         });
       });
 
-      seMoedas(arrayMoedas);
+      setMoedas(arrayMoedas);
       setMoedaSelecionada(arrayMoedas[0].key);
       setLoading(false);
     }
 
     loadMoedas();
   }, []);
+
+  async function converter(){
+    if(moedaBValor === 0 || moedaBValor === '' || moedaBValor === null){
+      return;
+    }
+    const response = await api.get(`/all/${moedaSelecionada}-BRL`);
+    console.log(response.data[moedaSelecionada].ask);
+
+    let resultado = (response.data[moedaSelecionada].ask * parseFloat(moedaBValor));
+    setValorConvertido(`${resultado.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`);
+    setValorMoeda(moedaBValor);
+    Keyboard.dismiss()
+  }
 
   if(loading){
     return(
@@ -51,12 +70,26 @@ export default function App(){
           />
           <View style={styles.areaValor}>
             <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
-            <TextInput placeholder="Digite o Valor!" keyboardType="numeric" style={styles.input}/>
+            <TextInput placeholder="Digite o Valor!" keyboardType="numeric" value={moedaBValor} onChangeText={(valor)=> setMoedaBValor(valor)} style={styles.input}/>
           </View>
         </View>
-        <TouchableOpacity style={styles.areaBotao} onPress={()=> {}}>
+        <TouchableOpacity style={styles.areaBotao} onPress={converter}>
           <Text style={styles.textoBotao}>Converter</Text>
         </TouchableOpacity>
+        {/* se o valor estiver com algo */}
+        {valorConvertido !== 0 && (
+          <View style={styles.areaResultado}>
+          <Text style={styles.valorConvertido}>
+            {valorMoeda} {moedaSelecionada}
+          </Text>
+          <Text style={styles.valorConvertido}>
+            Corresponde a:
+          </Text>
+          <Text style={styles.valorConvertido}>
+            {valorConvertido}
+          </Text>
+        </View>
+        )}
       </View>
     );
   }
@@ -109,5 +142,19 @@ const styles = StyleSheet.create({
     color: '#000',
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  areaResultado: {
+    width: '90%',
+    backgroundColor: '#fff',
+    marginTop: 34,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  valorConvertido: {
+   fontSize: 28,
+   color: '#000',
+   fontWeight: 'bold'
   },
 });
